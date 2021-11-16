4f5ee8947d934da8e778ecb7df728e417b302e28
ayylol <davidjgarciao@gmail.com>
2021-11-15 23:03:53 -0700

    Organized Room Scene

--- scripts/room.gd
+++ scripts/room.gd
@@ -1,20 +1,12 @@
-extends StaticBody2D
+extends Node2D
 
 var doors = []
 
-export var room_cleared = true
+export var room_cleared = false
 
 func _ready():
-	doors.push_back($TopDoor)
-	doors.push_back($BottomDoor)
-	doors.push_back($LeftDoor)
-	doors.push_back($RightDoor)
-	
-	#for door in $Doors.get_children():
-	#	doors.push_back(door)
-	#	door.open()
-	
-	for door in doors:
+	for door in $Doors.get_children():
+		doors.push_back(door)
 		door.open()
 
 func _on_RoomBounds_body_entered(body):
